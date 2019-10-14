6a920abf61dabb691925d335b756cbc52510aee4
raed <testuser@example.com>
2019-10-14 13:34:55 +0300

    bestgoalspage_user_completed

--- footclick_front/src/pages/BestGoalsPage_User/BestGoalsPage_User.js
+++ footclick_front/src/pages/BestGoalsPage_User/BestGoalsPage_User.js
@@ -1,14 +1,44 @@
 import React, { Component } from "react";
-//------------------------ scss link ----------------------------------------------------------------------------
+//------------------------ @start import components ----------------------------------------------------------------
+import NavBar from "../../components/NavBar/NavBar";
+import Footer from "../../components/Footer/Footer";
+//------------------------ @end import components ------------------------------------------------------------------
+
+//------------------------ @start import scss link ----------------------------------------------------------------------------
 import "../BestGoalsPage_User/BestGoalsPage_User.scss";
-//------------------------------------------------------------------------------------------------------------------
+//--------------------------@end import scss link----------------------------------------------------------------------------------------
 class BestGoalsPage_User extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
   render() {
-    return <div className="BestGoalsPage_User"></div>;
+    return (
+      <div className="BestGoalsPage_User">
+        <NavBar />
+        <div className="BestGoalsPage_User_Content">
+          <h1>Discover our Highlights</h1>
+          <hr className="Horizontal_Line" />
+          {/* --------------------------@start grid container--------------------------------------------- */}
+          <div className="BestGoalsPage_User_Content_Videos">
+            {/* --------------------------@start grid child--------------------------------------------- */}
+            <div className="BestGoalsPage_User_Content_Videos_Elements">
+              <div class="embed-responsive embed-responsive-16by9">
+                <iframe
+                  class="embed-responsive-item"
+                  src="https://www.youtube.com/embed/v64KOxKVLVg"
+                ></iframe>
+              </div>
+            </div>
+            {/* --------------------------@end grid child--------------------------------------------- */}
+          </div>
+          {/* --------------------------@end grid container--------------------------------------------- */}
+        </div>
+        <div className="Footer_BestGoalsPage_User">
+          <Footer />
+        </div>
+      </div>
+    );
   }
 }
 
--- footclick_front/src/pages/ContactPage/ContactPage.js
+++ footclick_front/src/pages/ContactPage/ContactPage.js
@@ -1,15 +1,71 @@
 import React, { Component } from "react";
 
-//------------------------ scss link ----------------------------------------------------------------------------
+//------------------------ @start import components ----------------------------------------------------------------
+import NavBar from "../../components/NavBar/NavBar";
+import Footer from "../../components/Footer/Footer";
+//------------------------ @end import components ----------------------------------------------------------------
+
+//------------------------ @start import scss link -------------------------------------------------------------------------
 import "../ContactPage/ContactPage.scss";
-//------------------------ ------------- ----------------------------------------------------------------------------
+//------------------------ @end import scss link----------------------------------------------------------------------------
 class ContactPage extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
   render() {
-    return <div className="ContactPage"></div>;
+    return (
+      <div className="ContactPage">
+        <NavBar />
+        {/* ------------------------ @start ContactContent ----------------------------------------------------------- */}
+        <div className="ContactContent">
+          {/* ------------------------ @start grid 1 ----------------------------------------------------------- */}
+          <div className="Map">
+            <img src="https://raw.githubusercontent.com/xuopled/react-google-map/HEAD/screenshots/react-google-map-exemple.png" />
+          </div>
+          {/* ------------------------ @end grid 1 ----------------------------------------------------------- */}
+
+          {/* ------------------------ @start grid2 ----------------------------------------------------------- */}
+          <div className="ContactForm">
+            <div class="shade">
+              <div class="blackboard">
+                <div class="form">
+                  <p>
+                    <label>Name: </label>
+                    <input type="text" />
+                  </p>
+                  <p>
+                    <label>Email: </label>
+                    <input type="text" />
+                  </p>
+                  <p>
+                    <label>Phone: </label>
+                    <input type="tel" />
+                  </p>
+                  <p>
+                    <label>Subject: </label>
+                    <input type="text" />
+                  </p>
+                  <p>
+                    <label>Message: </label>
+                    <textarea></textarea>
+                  </p>
+                  <p class="wipeout">
+                    <input type="submit" value="Send" />
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            {/* ------------------------ @end grid 2 ----------------------------------------------------------- */}
+          </div>
+          {/* ------------------------ @end ContactContent ---------------------------------------------------------------- */}
+        </div>
+        <div className="Footer_Contact">
+          <Footer />
+        </div>
+      </div>
+    );
   }
 }
 
