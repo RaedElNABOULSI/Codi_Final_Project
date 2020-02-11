0f23278dcf34bfa7f7ac47eb724b1f3c7de31e86
raed <testuser@example.com>
2020-02-11 20:28:00 +0200

    front_axios_modified

--- footclick_front/src/pages/BestGoalsPage_Player/BestGoalsPage_Player.js
+++ footclick_front/src/pages/BestGoalsPage_Player/BestGoalsPage_Player.js
@@ -20,10 +20,15 @@ class BestGoalsPage_Player extends Component {
 
   //------------------------ @start Fetching goal footage-------------------------------------------------------
   componentDidMount() {
-    axios.get("http://127.0.0.1:8000/api/bestgoal").then(res => {
-      console.log(res.data);
-      this.setState({ data: res.data });
-    });
+    axios
+      .get("http://127.0.0.1:8000/api/bestgoal")
+      .then(res => {
+        console.log(res.data);
+        this.setState({ data: res.data });
+      })
+      .catch(error => {
+        console.error(error);
+      });
   }
   //------------------------ @end Fetching goal gootage -----------------------------------------------------------
 
